6f20ff914202386e0bca9e9254fd1c5edcafb5ab
Edgar_Chacon_1548 <edgar.chacon@utec.edu.pe>
2025-12-17 20:11:06 -0500

    some program

---
+++ someProgram.rb
@@ -0,0 +1,7 @@
+# Specify the file path
+original_file = "D:\\Verificacion\\selfieDNI.jpeg"
+new_file = "D:\\Verificacion\\selfieDNI.jpg"
+
+# Rename the file
+File.rename(original_file, new_file)
+puts "File has been renamed to .jpg"
